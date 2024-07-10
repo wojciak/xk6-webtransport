e283aa3aec1edbfda943e9776d8e47946b82525e
Lukasz Wojciak <kontakt@wojciak.com.pl>
2024-07-10 13:33:19 +0200

    Update streams.go

--- streams.go
+++ streams.go
@@ -5,6 +5,9 @@ import (
 )
 
 func (c *Connection) OpenStream() int64 {
+	if c == nil || c.Session == nil {
+        	return 0
+	}
 	str, err := c.Session.OpenStream()
 	if err != nil {
 		log.Println("Stream error: " + err.Error())
@@ -16,7 +19,7 @@ func (c *Connection) OpenStream() int64 {
 }
 
 func (c *Connection) CloseStream() {
-	if c.activeStream != nil {
+	if c != nil && c.activeStream != nil {
 		c.activeStream.Close()
 		delete(c.streams, int64(c.activeStream.StreamID()))
 		c.activeStream = nil
@@ -24,7 +27,7 @@ func (c *Connection) CloseStream() {
 }
 
 func (c *Connection) CloseStreamById(id int64) {
-	if c.streams[id] != nil {
+	if c!=nil && c.streams[id] != nil {
 		c.streams[id].Close()
 		delete(c.streams, id)
 		if c.activeStream != nil && int64(c.activeStream.StreamID()) == id {
@@ -34,11 +37,15 @@ func (c *Connection) CloseStreamById(id int64) {
 }
 
 func (c *Connection) CloseAllStreams() {
-	for id := range c.streams {
-		c.CloseStreamById(id)
+	if c != nil {
+		for id := range c.streams {
+			c.CloseStreamById(id)
+		}
 	}
 }
 
 func (c *Connection) SetActiveStream(id int64) {
-	c.activeStream = c.streams[id]
+	if c != nil {
+		c.activeStream = c.streams[id]
+	}
 }
